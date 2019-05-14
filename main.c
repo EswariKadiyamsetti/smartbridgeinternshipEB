2691ccc709c7b5c5429102b45eddcdfb2d77e0cc
EswariKadiyamsetti <50614322+EswariKadiyamsetti@users.noreply.github.com>
2019-05-14 14:41:33 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,25 @@
+/*
+ * GccApplication1.c
+ *
+ * Created: 5/9/2019 2:15:56 PM
+ * Author : Prasanth Remala
+ */ 
+#ifndef F_CPU
+#define F_CPU 16000000UL
+#endif
+#include <avr/io.h>
+#include <util/delay.h>
+
+
+int main(void)
+{
+    DDRB= 0XFF
+    while (1) 
+    {
+	  PORTB = 0XFF;
+	  _delay_ms(100);
+	  PORTB = 0X00;
+	  _delay_ms(100);	
+    }
+}
+
