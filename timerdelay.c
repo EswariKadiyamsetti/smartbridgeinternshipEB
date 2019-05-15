b41edfb0b31a329bacd6c4478c4480e3fc5cdbe3
EswariKadiyamsetti <50614322+EswariKadiyamsetti@users.noreply.github.com>
2019-05-15 11:00:04 +0530

    Add files via upload

---
+++ timerdelay.c
@@ -0,0 +1,32 @@
+/*
+ * timerdelay.c
+ *
+ * Created: 5/15/2019 10:31:19 AM
+ * Author : Eswari
+ */ 
+
+#include <avr/io.h>
+#define LED PB0
+
+
+int main(void)
+{
+	uint8_t timerOverflowCount=0;
+	DDRB=0XFF;
+	TCNT0=0X00;
+	TCCR0=(1<<CS00)|(1<<CS02);
+    /* Replace with your application code */
+    while (1) 
+    {
+		while((TIFR&0X01)==0);
+		TCNT0=0X00;
+		TIFR=0X01;
+		timerOverflowCount++;
+		if(timerOverflowCount>=5)
+		{
+			PORTB^=(0X01<<LED);
+			timerOverflowCount=0;
+		}
+    }
+}
+
